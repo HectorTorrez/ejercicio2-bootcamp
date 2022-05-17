081e87e779aa3f4099538406fa35b169b1710b1c
Héctor Buruca <torrezhectorb@gmail.com>
2022-05-16 21:09:02 -0600

    exercice 2.1

---
+++ index.js
@@ -0,0 +1,17 @@
+let list = [
+    "Hector",
+    22,
+    true,
+    new Date('04-18-00'),
+    {
+
+        book: {
+            "Titulo": "Muerte",
+            "Autor": "Hector",
+            "Fecha": new Date('02-20-22'),
+            "url": "inventada.com"
+        }
+    }
+]
+
+console.log(list);
